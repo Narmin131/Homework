7005f46f972eca4c191685d094bc73463be3ae6e
Narmin <mnermin064@gmail.com>
2022-03-02 15:27:27 +0400

    added js functions

--- Example-3/assets/script/script.js
+++ Example-3/assets/script/script.js
@@ -1,9 +1,24 @@
-$(document).ready(function(){
-    $(".solution").waypoint(function(direction){
-        if(direction=="down"){
-            $("header").addClass(".fixed-nav");
-        }else{
-            $("header").removeClass(".fixed-nav");
-        }
-    })
+// Fixed nav
+$(document).ready(function() {
+  $(window).scroll(function () { 
+    console.log($(window).scrollTop());
+
+    if ($(window).scrollTop() > 50) {
+      $('header').addClass('fixed-nav');
+      // $('.blue').css('color',"black");
+      // $('.imglogo').css('src',"././assets/img/logo.png");
+    }
+
+    if ($(window).scrollTop() < 51) {
+      $('header').removeClass('fixed-nav');
+    }
+  });
 });
+
+// Faq accordion added
+ const faqs = document.querySelectorAll(".faq");
+ faqs.forEach((faq) =>{
+     faq.addEventListener("click",() => {
+       faq.classList.toggle("active");
+     });
+ });
